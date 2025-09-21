4be1986974eaa6173781e75d6635c9128628878a
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2025-09-21 06:26:12 +0500

    1

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -12,6 +12,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+// import 'package:sqlite3/sqlite3.dart';
 
 class AppDatabase {
   static Database? _database; // The actual database instance.
